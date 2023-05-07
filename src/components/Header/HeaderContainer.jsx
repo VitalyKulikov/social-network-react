7bd36a108870ad28bc3ffda0e3542c0b568e2d61
Vitaly Kulikov <vitaly.kulikov90@gmail.com>
2023-05-07 12:25:43 +0300

    auth login

---
+++ src/components/Header/HeaderContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import h from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={h.header}>
+      <h1>Social Network</h1>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/redux/auth-reducer.js
@@ -0,0 +1,33 @@
+const ADD_POST = 'ADD_POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
+const SET_USERS_PROFILE = 'SET_USERS_PROFILE';
+
+let initialState = {
+    postsData: [
+        { id: 1, message: `How are you?`, like: 15, },
+        { id: 2, message: `It's my first post!`, like: 20, }
+    ],
+    newPostText:'',
+    profile: null,
+}
+
+const profilePageReducer =  (state = initialState, action) => {
+
+switch (action.type) {
+    case ADD_POST:
+        let newPost = { id: 5, message: state.newPostText,like: 0 };
+        return {...state, postsData: [...state.postsData,newPost], newPostText: ''}
+    case UPDATE_NEW_POST_TEXT: {
+        return {...state, newPostText: action.newText}}
+    case SET_USERS_PROFILE: {
+            return {...state, profile: action.profile}}
+        default:
+            return state;
+    } 
+}
+
+export const addPostActionCreator = () => ({type: ADD_POST });
+export const setUsersProfile = (profile) => ({type: SET_USERS_PROFILE, profile});
+export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text});
+
+export default profilePageReducer;
